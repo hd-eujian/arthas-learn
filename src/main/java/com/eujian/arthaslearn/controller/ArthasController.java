1a884f5ad7e4abce31ca020e6c5ca67521e1c899
yeyongjian <yeyongjian@gac-nio.com>
2020-05-22 16:31:41 +0800

    tijaio

--- src/main/java/com/eujian/arthaslearn/controller/ArthasController.java
+++ src/main/java/com/eujian/arthaslearn/controller/ArthasController.java
@@ -1,6 +1,11 @@
 package com.eujian.arthaslearn.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,12 @@ public class ArthasController {
     public String arthas(String a){
         System.out.println(a);
         return a;
+    }
+
+    @PostMapping("/post")
+    public ObjectNode post(@RequestBody ObjectNode objectNode){
+        objectNode.put("a","q");
+        System.out.println(objectNode);
+        return objectNode;
     }
 }
