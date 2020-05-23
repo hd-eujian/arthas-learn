cd563f8cc31440e3875acb352a28021e574c8c8d
yeyongjian <yeyongjian@gac-nio.com>
2020-05-23 17:23:48 +0800

    git commit

--- src/main/java/com/eujian/arthaslearn/controller/MyService.java
+++ src/main/java/com/eujian/arthaslearn/controller/MyService.java
@@ -4,6 +4,11 @@ public class MyService {
 
     public String send(){
         System.out.println("send被调用了");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "send";
     }
 
